0ebbaf1542eb90de9d065b0dfcca0146f2cf4962
4041443 <130870428+4041443@users.noreply.github.com>
2023-04-26 20:11:27 +0300

    Add files via upload

---
+++ loadscreen_video/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+loadscreen 'html/index.html'
+
+files {
+    'html/index.html',
+	'html/css/style.css',
+	'html/js/main.js',
+    'html/assets/bgvideo.mp4',
+	'html/assets/music.mp3'
+}
+
+loadscreen_manual_shutdown 'yes'
+
+client_script 'client/client.lua'
